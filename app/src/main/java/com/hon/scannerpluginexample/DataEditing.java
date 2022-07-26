ec8394b08536afc04abcf0fedb3a0c834a36f214
NIK.DZL <cxnt48@gmail.com>
2022-07-26 12:58:50 +0200

    Inter character delay added

--- app/src/main/java/com/hon/scannerpluginexample/DataEditing.java
+++ app/src/main/java/com/hon/scannerpluginexample/DataEditing.java
@@ -26,10 +26,11 @@ public class DataEditing extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        String ScanResult = intent.getStringExtra("data");//Read the scan result from the Intent
+        String scanResult = intent.getStringExtra("data");//Read the scan result from the Intent
         Bundle bundle = new Bundle();
 
         //convert data to hex string
+        /*
         byte[] digits = ScanResult.getBytes();
         Formatter formatter = new Formatter();
         for (byte b : digits) {
@@ -39,8 +40,25 @@ public class DataEditing extends BroadcastReceiver {
 
         // add prefix
         hex = prefix+hex;
-        bundle.putString("data", hex);
-        //bundle.putString("delayStr", "{d2500}");
+        */
+
+        String INTER_CHAR_DELAY = "{d500}";
+        StringBuilder sb_data = new StringBuilder();
+        StringBuilder sb_delay= new StringBuilder();
+        char[] chars = scanResult.toCharArray();
+        for (char ch: chars) {
+            sb_data.append(ch);
+            sb_data.append(INTER_CHAR_DELAY);
+            sb_delay.append(INTER_CHAR_DELAY+";");
+        }
+
+
+        String delayedScannedData = sb_data.toString();
+        String delayString = sb_delay.toString();
+        Toast.makeText(context, "<"+delayedScannedData+">" , Toast.LENGTH_LONG).show();
+
+        bundle.putString("data", delayedScannedData);
+        bundle.putString("delayStr", delayString);
 
         setResultExtras(bundle);
     }
